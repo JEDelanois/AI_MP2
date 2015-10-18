d550421cc2deb0746ac0a1c59bd9d2e81a3d2d3f
JEDelanois <jedelanois@gmail.com>
2015-10-18 12:09:03 -0500

    set up first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AI_MP2
+//
+//  Created by Erik on 10/18/15.
+//  Copyright (c) 2015 Erik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
