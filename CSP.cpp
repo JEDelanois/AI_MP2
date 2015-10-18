9bef696000ddedee03284fe995f245f22550b26f
ddelane2 <ddelane2@illinois.edu>
2015-10-18 15:48:06 -0500

    hash function

--- CSP.cpp
+++ CSP.cpp
@@ -15,5 +15,65 @@ vector<string>&  DataBase::operator[] (const string nString)
 
 int DataBase::hash(string s)
 {
-    
+	if (s == adjective)
+		return 0;
+	if (s == adverb)
+		return 1;
+	if (s == animal)
+		return 2;
+	if (s == body)
+		return 3;
+	if (s == clothing)
+		return 4;
+	if (s == color)
+		return 5;
+	if (s == computer)
+		return 6;
+	if (s == conjunction)
+		return 7;
+	if (s == container)
+		return 8;
+	if (s == emotion)
+		return 9;
+	if (s == family)
+		return 10;
+	if (s == food)
+		return 11;
+	if (s == furniture)
+		return 12;
+	if (s == game)
+		return 13;
+	if (s == health)
+		return 14;
+	if (s == interjection)
+		return 15;
+	if (s == job)
+		return 16;
+	if (s == kitchen)
+		return 17;
+	if (s == language)
+		return 18;
+	if (s == math)
+		return 19;
+	if (s == money)
+		return 20;
+	if (s == music)
+		return 21;
+	if (s == nature)
+		return 22;
+	if (s == noun)
+		return 23;
+	if (s == number)
+		return 24;
+	if (s == palindrome)
+		return 25;
+	if (s == pronoun)
+		return 26;
+	if (s == shape)
+		return;
+	if (s == transportation)
+		return 27;
+	if (s == verb)
+		return 28;
+
 }
