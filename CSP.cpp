da27a7f94101b56050e5795427245853ee3ec7ad
ddelane2 <ddelane2@illinois.edu>
2015-10-18 15:51:47 -0500

    hash function

--- CSP.cpp
+++ CSP.cpp
@@ -15,65 +15,65 @@ vector<string>&  DataBase::operator[] (const string nString)
 
 int DataBase::hash(string s)
 {
-	if (s == adjective)
+	if (s == "adjective")
 		return 0;
-	if (s == adverb)
+	if (s == "adverb")
 		return 1;
-	if (s == animal)
+	if (s == "animal")
 		return 2;
-	if (s == body)
+	if (s == "body")
 		return 3;
-	if (s == clothing)
+	if (s == "clothing")
 		return 4;
-	if (s == color)
+	if (s == "color")
 		return 5;
-	if (s == computer)
+	if (s == "computer")
 		return 6;
-	if (s == conjunction)
+	if (s == "conjunction")
 		return 7;
-	if (s == container)
+	if (s == "container")
 		return 8;
-	if (s == emotion)
+	if (s == "emotion")
 		return 9;
-	if (s == family)
+	if (s == "family")
 		return 10;
-	if (s == food)
+	if (s == "food")
 		return 11;
-	if (s == furniture)
+	if (s == "furniture")
 		return 12;
-	if (s == game)
+	if (s == "game")
 		return 13;
-	if (s == health)
+	if (s == "health")
 		return 14;
-	if (s == interjection)
+	if (s == "interjection")
 		return 15;
-	if (s == job)
+	if (s == "job")
 		return 16;
-	if (s == kitchen)
+	if (s == "kitchen")
 		return 17;
-	if (s == language)
+	if (s == "language")
 		return 18;
-	if (s == math)
+	if (s == "math")
 		return 19;
-	if (s == money)
+	if (s == "money")
 		return 20;
-	if (s == music)
+	if (s == "music")
 		return 21;
-	if (s == nature)
+	if (s == "nature")
 		return 22;
-	if (s == noun)
+	if (s == "noun")
 		return 23;
-	if (s == number)
+	if (s == "number")
 		return 24;
-	if (s == palindrome)
+	if (s == "palindrome")
 		return 25;
-	if (s == pronoun)
+	if (s == "pronoun")
 		return 26;
-	if (s == shape)
-		return;
-	if (s == transportation)
+	if (s == "shape")
 		return 27;
-	if (s == verb)
+	if (s == "transportation")
 		return 28;
+	if (s == "verb")
+		return 29;
 
 }
